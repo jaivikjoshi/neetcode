3396864bd1013d538a5984d119160338164ff752
Jaivik <jaivikjoshi7@gmail.com>
2026-05-20 20:01:57 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output = {}
+        i = 0
+
+        for word in strs:
+            check = {}
+            for char in word:
+                if char not in check:
+                    check[char] = 1
+                else:
+                    check[char] += 1
+            
+            key = tuple(sorted(check.items()))
+
+            if key not in output:
+                output[key] = []
+
+            output[key].append(word)
+
+            
+        return list(output.values())
+
+
+              
+        
+
+
+        return [[""]]      
+
